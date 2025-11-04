cee00a0cbc99425b78d09684dc1ca5391dffb56a
Aminabsk <aminahamzic907@gmail.com>
2025-11-04 08:19:17 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Laboratoire prêt !");
+
+const nom = "Amina";
+
+let age = 17;
+
+//nom = "Hamzic";
+//script.js:4 Uncaught TypeError: Assignment to constant variable.
+
+   
+age = 18
+console.log(age);
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "12345"; // Changez cette valeur pour tester
+
+if ( motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
